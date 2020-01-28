2bef05d6f36a9eaf3a3ae2bbbada709d5a00af0f
bounce <johan@waytogo.se>
2020-01-28 22:22:32 +0100

    Adding more dividables?

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,11 @@
 def fizz_buzz(number)
    if number%3 == 0
     'Fizz!'
+    if mumber%5 == 0
+    'Buzz!'
+    if number%15 == 0
+        'FizzBuzz!'
+    
    else
     number
    end
-end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,7 +4,13 @@ describe "fizz_buzz" do
     it 'returns 1 if number is 1' do
         expect(fizz_buzz(1)).to eq 1
     end
-    it 'returns fizz! if number is dividable by 3' do
+    it 'returns Fizz! if number is dividable by 3' do
         expect(fizz_buzz(3)).to eq 'Fizz!'
+    end
+    it 'returns Buzz! if number is dividable by 5' do
+        expect(fizzbuzz(3)).to eq 'Buzz!'
+    end
+    it 'returns FizzBuzz! if number is dividable by 15' do
+        expect(fizzbuzz(3)).to eq 'FizzBuzz!'
     end
 end
